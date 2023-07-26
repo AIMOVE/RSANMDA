a21ec84193a74db56337cebdba718cd07588337a
罗龙飞 <43193538+AIMOVE@users.noreply.github.com>
2023-07-26 16:35:43 +0800

    import_dataset.py

--- code/import_dataset.py
+++ code/import_dataset.py
@@ -1,25 +1,5 @@
-# -*- coding: utf-8 -*-
 import numpy as np
 import random
-from sklearn.cluster import KMeans
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.svm import SVC
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import GradientBoostingClassifier
-
-from sklearn.neural_network import MLPClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.gaussian_process import GaussianProcessClassifier
-from sklearn.gaussian_process.kernels import RBF
-from sklearn.naive_bayes import GaussianNB
-from sklearn.model_selection import GridSearchCV
-
-
-#from keras.tensorflow.models import Sequential
-#from keras.tensorflow.layers import Dense, Dropout
-import xlrd
-
 
 def import_dis_mir_vec(data_path):
     print('\nDisease comprehensive similarity ...\n')
